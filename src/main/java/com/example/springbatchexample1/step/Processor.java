94390b6e2b589eb94b4acc7651f3545d2d7e8671
abinashmandi123 <abinashmandi6@gmail.com>
2021-10-11 20:33:25 +0530

    Added another print message

--- src/main/java/com/example/springbatchexample1/step/Processor.java
+++ src/main/java/com/example/springbatchexample1/step/Processor.java
@@ -7,7 +7,7 @@ public class Processor implements ItemProcessor<String, String>{
 	@Override
 	public String process(String data) throws Exception {
 		System.out.println("This is processor");
-		System.out.println("This is an error message");
+		System.out.println("This is an message");
 		return data.toUpperCase();
 	}
 }
